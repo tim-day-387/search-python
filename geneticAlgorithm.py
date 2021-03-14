4a8f31ea3171570a175072defb361a96560109cc
Stevie93 <paa100193@gmail.com>
2021-03-14 17:42:28 -0400

    One typo corrected
    Tried doing a debug, it still seems autoadjust is a bit problematic

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -115,7 +115,7 @@ def getMutateList():
     ret=[]
     for queen in range(9):
         if RNG.random() <=0.04:
-            moveSize=abs(RNG.randrange(mutate+1)+RNG.randint(-1*mutateSize,mutateSize)) #bias to smaller values.
+            moveSize=abs(RNG.randrange(mutateSize+1)+RNG.randint(-1*mutateSize,mutateSize)) #bias to smaller values.
             if moveSize==0:
                 moveSize=1
             elif moveSize>8:
