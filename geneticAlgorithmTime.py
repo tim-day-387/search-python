8b0a317372360da7aa241780769484ba63062a92
Romaji Milton Amulo <nxt101@gmail.com>
2021-03-16 12:42:25 -0400

    Update geneticAlgorithmTime.py
    added measure of when best found, fixed various bugs, and improved parent selection.

--- geneticAlgorithmTime.py
+++ geneticAlgorithmTime.py
@@ -1,4 +1,5 @@
 from board import board, moveException
+from fractions import Fraction as fr #used to invert the costs into fitnesses without roundoff error
 import random as RNG
 import time
 #code by Romaji.
@@ -39,10 +40,8 @@ Then, performs any mutations (listed in [queenNumber, amount] format,
 where queen number is 0 to size, 
 and amount is how much to move by (if possible. Otherwise, goes to an open space.).
 Finally, it computes the proper movement cost from the original.
-
 Then repeats with the alternate offspring.
 Offspring 1 uses mutations 1, offspring 2 uses mutations 2
-
 Picks columns at random for mutation"""
     division=[False] #list for which offspring gets which column. First offspring always gets first colum.
     allFalse=True #make sure there's at least one true.
@@ -171,14 +170,16 @@ else:
     startBoard.showState()
 
 print("generating population...")
-parents=[]
+parents=[startBoard.copy()] #make it so it can't be worse than the unmodified board.
 while len(parents) < populationSize:
     temp=startBoard.copy()
     for i in range(4*size): #this is slow, but... it's only run once per member of the starting population.
         moves=temp.listMoves()
         move=RNG.choice(moves)
         temp.moveQueen(*move)
-    parents.append(temp)
+    parent=startBoard.copy() #this is to  fix that all those moves will throw the cost crazy high
+    parent.autoAdjust(temp) #kinda embarrassing I forgot to do this the first time
+    parents.append(parent)
 print("parents generated.")
 parents.sort(key=lambda this:this.getCost()) #sort by cost, smallest to largest
 print("best so far")
@@ -191,16 +192,48 @@ else:
 generation=1
 timeOfLastUpdate=time.process_time()
 endAfter=time.process_time()+allowedTime #marks the point when this should stop, by taking the process clock
+startTime=endAfter-allowedTime
+lastImprovement=0.0
 #before the loop, and adding the number of seconds allowed
 bestOf=[parents[0]] #saves the best of each generation for later display
 while time.process_time() < endAfter: #has the clock hit the ending yet?
     kids=parents[:preserve] #keep the top however many
     #culling step
     parents=parents[:-1*cull]
+    #big change to this version: make it so the actual cost of the parents is used in generating rank,
+    #not just their position.
+    invertedCost=[] #takes the "fitness" function role
+    totalInvertedCost=fr() #makes the starting total inverted cost be zero
+    for par in parents:
+        temp=fr(1,int(par.getCost()))#the int is just so that I'm sure the fraction class won't complain
+        invertedCost.append(temp)
+        totalInvertedCost+=temp
+        
     while len(kids) < populationSize and time.process_time() < endAfter: #makes sure to break out if spent too much time
-        parentIndex1= int((RNG.uniform(1,(len(parents))**2))**0.5)-1 #biases more to lower ranked parents. Cannot be the last parent
-        parentIndex2= int((RNG.uniform((parentIndex1+1)**2,(len(parents)+1)**2))**0.5)-1 #can be the last parent. Higher than par1
-        #get the parents
+        #get the parent IDs
+        #first parent
+        parentIndex1=0
+        #make a random fraction between 0 and just less than the total inverted cost
+        generatingNumber=fr(RNG.randrange(totalInvertedCost.numerator),totalInvertedCost.denominator)
+        while generatingNumber > invertedCost[parentIndex1]: #will continue until the generating number is small enough.
+            generatingNumber -= invertedCost[parentIndex1] #remove this element, to represent moving past it
+            parentIndex1 +=1 #move forward again
+        #now parentIndex 1 is set, but something special is done to make sure the same parent isn't picked twice
+        invCostSubPar1=totalInvertedCost-invertedCost[parentIndex1] #remove from the roulet parent 1's index
+        parentIndex2=0
+        #now make the generating number
+        generatingNumber=fr(RNG.randrange(invCostSubPar1.numerator),invCostSubPar1.denominator)
+        while True: #writing this differently helps clarify
+            if parentIndex2==parentIndex1: #if the indexes are the same, proceed on to skip the parentIndex1 slice
+                parentIndex2+=1
+                continue
+            #otherwise, continue with checking if this in the slice
+            if generatingNumber <= invertedCost[parentIndex2]:
+                break #if the generating number is small enough, we're done.
+            #otherwise, move on to the next index
+            generatingNumber -= invertedCost[parentIndex2]
+            parentIndex2+=1
+        #Now get them from the list
         par1=parents[parentIndex1]
         par2=parents[parentIndex2]
         #make the mutation lists
@@ -214,11 +247,14 @@ while time.process_time() < endAfter: #has the clock hit the ending yet?
 ##    print("generation",generation,"best result")
 ##    kids[0].showState()
     bestOf.append(kids[0])
+    if bestOf[-2].getCost() >bestOf[-1].getCost(): #has it improved?
+        lastImprovement=time.process_time()-startTime
     #see if update is needed.
     if time.process_time()>=timeOfLastUpdate+timeTillUpdate:
             timeOfLastUpdate=time.process_time() #save time now, before the screen draw
             if quickMode=="y":
-                print(generation,endAfter-timeOfLastUpdate,bestOf[-1].getCost(),sep=",")
+                print(generation,endAfter-timeOfLastUpdate,lastImprovement,
+                      bestOf[-1].getCost(),sep=",")
             else:
                 print("generation:",generation,"time remaining:",endAfter-timeOfLastUpdate)
                 bestOf[-1].showState()
@@ -237,10 +273,12 @@ elif q=="s":
     midCost=bestOf[(populationSize//2)-1].getCost()
     firstCost=bestOf[0].getCost()
     print("at start",firstCost,"then half way",midCost,"with finally ending on",lastCost)
-    if (lastCost/midCost) > (midCost/firstCost):
+    if (lastCost/midCost) < (midCost/firstCost):
         print("it really picked up at the end")
+    elif (lastCost/midCost) ==(midCost/firstCost):
+        print("it was constant")
     else:
-        print("the first half was at least as good as the second")
+        print("the start was the best.")
 elif q=="f":
     bestOf[-1].showState()
 else:
